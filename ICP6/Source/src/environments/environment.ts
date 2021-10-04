01e6736fa6a44c8ab2221a7a24d9c40528ab4edb
bhuvan454 <36909603+bhuvan454@users.noreply.github.com>
2021-10-04 00:23:04 -0500

    summary

---
+++ ICP6/Source/src/environments/environment.ts
@@ -0,0 +1,20 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  foursquare_id : 'DM3S41C42BU5MEGRRGPZ0PHFEVDEPXQDJSUOJUZHZOXPQ0TY',
+  foursquare_key :  'KSVKC2OFIBVVNXYMXNKV53RFRZPO4PDPPOA2KZ0AXK4BS3VV',
+  edamam_id : '6f914a8b',
+  edamam_key : '724b22fae07f9e673e8843b287045da8',
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
